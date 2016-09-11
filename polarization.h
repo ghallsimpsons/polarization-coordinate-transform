e5a644ed397a3a24680ccae46f16603e4e70db18
Grantland Hall <grantlandhall@grantlandhall.com>
2016-09-11 02:58:21 -0400

    Fix indentation

--- polarization.h
+++ polarization.h
@@ -9,4 +9,4 @@ double pol_angle_gal_to_eq(double l, double b, double pol_in);
 double pol_angle_eq_to_gal(double alpha, double delta, double pol_in);
 
 double pol_angle_coord_transform(double pole_theta, double pole_phi,
-								 double pix_theta, double pix_phi, double pol_in);
+                                 double pix_theta, double pix_phi, double pol_in);
